67a3301a440540cafd84c3666579b251da1918cc
Dmytro Avdieienko <avday.09@gmail.com>
2025-08-25 13:04:57 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -2,16 +2,15 @@ import './style.scss';
 import { animate, inView, scroll } from "motion";
 
 const main = document.getElementsByClassName("main").item(0);
+const content = document.getElementsByClassName("main__content").item(0);
 const title = document.getElementsByClassName('main__title').item(0);
 const note = document.getElementsByClassName('main__note').item(0);
-const image = document.getElementsByClassName('main__image').item(0);
-const link = document.getElementsByClassName('main__link').item(0);
-const blur = document.getElementsByClassName('blur').item(0);
+const background = document.getElementsByClassName('main__background').item(0);
 
-// scroll(animate(note, {y: 200}), { target: main });
-scroll(animate(image, {y: -500}), { target: main, offset: ['start 0.2', 'start start'] });
-scroll(animate(blur, {y: 0}), { target: main, offset: ['start 0.2', 'start start'] });
-
+scroll(animate(note, {display: 'none'}), { target: main, offset: ['0px', '500px'] });
+scroll(animate(title, {display: 'none'}), { target: main, offset: ['start 0.2', 'start start'] });
+scroll(animate(content, {top: 0}), { target: main, offset: ['0px', '500px'] });
+scroll(animate(background, {opacity: 0}), { target: main, offset: ['0px', '500px'] });
 
 const boxes = document.getElementsByClassName('about__box');
 
